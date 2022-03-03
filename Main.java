3eb12d73e5dc906c4f82ab5e48e47a0c2d59d1dc
valnicomex <90493902+valnicomex@users.noreply.github.com>
2022-03-03 21:01:58 +0000

    main, editing formating + implementing actual game

--- Main.java
+++ Main.java
@@ -6,15 +6,39 @@ public class Main {
 
   
   public static void main(String[] args) {
+
+    //scanner init
     Scanner sc = new Scanner(System.in);
+    
+    //array list: temp
     ArrayList<String> nothing = new ArrayList<String>();
     nothing.add("something");
+
+    //init obj + results
+    ArrayList<String> results = new ArrayList<String>();
     Wordle test = new Wordle(nothing);
-    
-    System.out.println("enter your guess");
-    String myLine = sc.nextLine();
-    
-    System.out.println(test.guess(myLine, 0));
+    int i;
+
+
+    //actual game starting
+    System.out.println("enter your guess (only enter five vaild letters: a-z no caps)");
+    for(i = 0; i < 6; i++) {
+      
+      String myLine = sc.nextLine();
+
+      results.add(test.guess(myLine, i));
+      System.out.println(results.get(i));
+      if(results.get(i).equals("✓✓✓✓✓")) {
+        i = 10;
+      }
+        
+    }
+
+    //end aka results
+    if(i == 10)
+      System.out.println("good job, you got the word");
+    else
+      System.out.println("oh no, you lost. the word was");
     
   }
 }
