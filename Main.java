7858269048aebf949b550f3ae7eff5ab1c9011ee
val <90493902+valnicomex@users.noreply.github.com>
2022-03-08 21:06:18 +0000

    working -> not more errors in anyway

--- Main.java
+++ Main.java
@@ -5,15 +5,10 @@ import java.io.*;
 public class Main {
 
   public static void fileToArrayList(FileInputStream fis, ArrayList<String> nothing) {
-    try {
-      Scanner sc = new Scanner(fis);
-      while (sc.hasNextLine())
-        nothing.add(sc.nextLine());
-      sc.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    Scanner sc = new Scanner(fis);
+    while (sc.hasNextLine())
+    nothing.add(sc.nextLine());
+    sc.close();
   }
 
   public static void main(String[] args) {
@@ -33,15 +28,16 @@ public class Main {
   boolean ending = true;
 
   // temp to test ending easier
-  System.out.println(test.getWord());
+  //System.out.println(test.getWord());
 
   // actual game starting
-  System.out.println("enter your guess (only enter five vaild letters: a-z no caps, any extra letters will be disregarded and shorter answers will count)");for(i=0;i<6;i++)
-  {
+  System.out.println("enter your guess (only enter five vaild letters: a-z no caps, any extra letters will be disregarded and shorter answers will count)");
+    System.out.println(":) = letter in correct spot \nO = letter in word, but not in correct spot \nX = letter is not in word");
+  Scanner sc = new Scanner(System.in);
+  for(i=0;i<6;i++) {
       // user input
-      Scanner sc = new Scanner(System.in);
+      
       String myLine = sc.nextLine();
-      sc.close();
 
       results.add(test.guess(myLine, i));
       System.out.println(results.get(i));
@@ -53,7 +49,11 @@ public class Main {
       }
 
     }
+    
 
   // end aka results
   if(ending)System.out.println("oh no, you lost. the word was: "+test.getWord());
-}}
+  sc.close();
+  }
+  
+}
